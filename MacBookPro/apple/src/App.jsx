c19e06c9a4f72fd5432596c2ccc5dfa47fa457b2
Nabin <sharmanabin.0910@gmail.com>
2025-07-13 11:58:21 +0530

    Completed making and rendering the 3D model

--- MacBookPro/apple/src/App.jsx
+++ MacBookPro/apple/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Canvas} from '@react-three/fiber';
 import './styles.css'
-import { Environment , OrbitControls } from '@react-three/drei';
+import { Environment , OrbitControls , ScrollControls } from '@react-three/drei';
 import MacContainer from './MacContainer';
 
 const App = () => {
-  return (
+  return (  
+    <div>
     <Canvas camera={{fov :12 , position : [0, -10 ,220]}}>
         <OrbitControls/>
         <Environment
@@ -13,8 +14,12 @@ const App = () => {
             'https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr'
           ]}
         />
-        <MacContainer/>
+        <ScrollControls pages = {3}>
+           <MacContainer/>
+        </ScrollControls>
+       
     </Canvas> 
+    </div>
   );
 }
 
