231483042100d8d3fa87d1be6b775a88a9cb84e8
Nabin <sharmanabin.0910@gmail.com>
2025-07-13 11:47:21 +0530

    creating the 3D object

--- MacBookPro/apple/src/App.jsx
+++ MacBookPro/apple/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import {Canvas} from '@react-three/fiber';
 import './styles.css'
-import { OrbitControls } from '@react-three/drei';
+import { Environment , OrbitControls } from '@react-three/drei';
 import MacContainer from './MacContainer';
 
 const App = () => {
   return (
-    <Canvas>
+    <Canvas camera={{fov :12 , position : [0, -10 ,220]}}>
         <OrbitControls/>
+        <Environment
+          files = {[
+            'https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr'
+          ]}
+        />
         <MacContainer/>
     </Canvas> 
   );
--- MacBookPro/apple/src/MacContainer.jsx
+++ MacBookPro/apple/src/MacContainer.jsx
@@ -1,12 +1,19 @@
 import { useGLTF } from '@react-three/drei'
+import * as THREE from 'three';
 import React from 'react'
 
 const MacContainer = () => {
-  let model = useGLTF('./mac.glb')
+  let model = useGLTF('./mac.glb');
+  let meshes = {};
+  model.scene.traverse((e) =>{
+     meshes[e.name] = e;
+    });
+
+   meshes.screen.rotation.x = THREE.MathUtils.degToRad(180);
   return (
-    <mesh>
-        <boxGeometry/>
-    </mesh>
+    <group position={[0 , -10 , 20]}>
+    <primitive object={model.scene} />
+    </group>
   )
 }
 
