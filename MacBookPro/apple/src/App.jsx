c5f8581d711f152d20fff63d4e4851dc69f19a89
Nabin <sharmanabin.0910@gmail.com>
2025-07-13 12:34:09 +0530

    Designing the header

--- MacBookPro/apple/src/App.jsx
+++ MacBookPro/apple/src/App.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
-import {Canvas} from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber';
 import './styles.css'
-import { Environment , OrbitControls , ScrollControls } from '@react-three/drei';
+import { Environment, OrbitControls, ScrollControls } from '@react-three/drei';
 import MacContainer from './MacContainer';
 
 const App = () => {
   return (  
     <div className='w-full h-screen'>
-      <div className='absolute text-white top-32 left-1/2 -translate-x-1/2 font-["Helvetica_Now_Display"]'>
-        <h3 className='text-7xl tracking-tighter font-[700]'>Macbook Pro</h3>
-        <h5 className='text-center'>Oh so Pro!</h5>
-        <p className='text-center w-3/4 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio hic exercitationem a molestias.</p>
+      <div className='absolute text-white top-40 left-1/2 -translate-x-1/2 transform text-center font-["Poppins"]'>
+        <h3 className='masked text-7xl tracking-tighter font-[700]'>Macbook Pro</h3>
+        <h5 className='text-lg mt-2'>Oh so Pro!</h5>
+        <p className='w-[80%] mx-auto mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio hic exercitationem a molestias.</p>
       </div>
-    <Canvas camera={{fov :12 , position : [0, -10 ,220]}}>
-        <OrbitControls/>
+
+      <Canvas camera={{ fov: 12, position: [0, -10, 220] }}>
+        <OrbitControls />
         <Environment
-          files = {[
+          files={[
             'https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr'
           ]}
         />
-        <ScrollControls pages = {3}>
-           <MacContainer/>
+        <ScrollControls pages={3}>
+          <MacContainer />
         </ScrollControls>
-       
-    </Canvas> 
+      </Canvas> 
     </div>
   );
 }
 
-export default App
+export default App;
